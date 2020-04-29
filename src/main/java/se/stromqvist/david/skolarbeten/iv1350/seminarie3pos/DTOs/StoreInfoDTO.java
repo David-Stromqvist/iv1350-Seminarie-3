ab7521d9c88f0f6700443468ed42802400cc7a8e
David Strömqvist <55108496+David-Stromqvist@users.noreply.github.com>
2020-04-29 15:28:48 +0200

    Added comments

--- src/main/java/se/stromqvist/david/skolarbeten/iv1350/seminarie3pos/DTOs/StoreInfoDTO.java
+++ src/main/java/se/stromqvist/david/skolarbeten/iv1350/seminarie3pos/DTOs/StoreInfoDTO.java
@@ -1,7 +1,8 @@
 package se.stromqvist.david.skolarbeten.iv1350.seminarie3pos.DTOs;
 
 /**
- *
+ * Hardcoded information about the Store.
+ * Should in reality be read from a file.
  * @author David
  */
 public class StoreInfoDTO {
