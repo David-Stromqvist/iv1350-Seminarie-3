782b44a0dec9c150834394319643493e2f995c90
David Strömqvist <55108496+David-Stromqvist@users.noreply.github.com>
2020-04-29 16:40:02 +0200

    Fixed price showing

--- src/main/java/se/stromqvist/david/skolarbeten/iv1350/seminarie3pos/DTOs/SoldItemDTO.java
+++ src/main/java/se/stromqvist/david/skolarbeten/iv1350/seminarie3pos/DTOs/SoldItemDTO.java
@@ -1,5 +1,7 @@
 package se.stromqvist.david.skolarbeten.iv1350.seminarie3pos.DTOs;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import se.stromqvist.david.skolarbeten.iv1350.seminarie3pos.util.*;
 
 /**
@@ -9,11 +11,13 @@ import se.stromqvist.david.skolarbeten.iv1350.seminarie3pos.util.*;
 public class SoldItemDTO {
     public final ItemDTO item;
     public final Amount amount;
+    public final BigDecimal priceForAllItems;
     
-    public SoldItemDTO(ItemDTO item, Amount amount)
+    public SoldItemDTO(ItemDTO item, Amount amount, BigDecimal priceForAllItems)
     {
         this.item = item;
         this.amount = amount;
+        this.priceForAllItems = priceForAllItems.setScale(2, RoundingMode.HALF_UP);
     }
 }
 
